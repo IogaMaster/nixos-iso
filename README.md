4775759fa8e6feb0076538833a5d8b9a88fac409
IogaMaster <iogamastercode@gmail.com>
2022-11-13 11:00:25 -0700

    :tada: Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# IogaMaster's NixOs installation ISO:
+Some changes have been made from the standard ISO.
+
+Replace `wpa_supplicant` with `NetworkManager` to make WIFI connection easier.
+
---
+++ iso.nix
@@ -0,0 +1,8 @@
+{ config, pkgs, ... }:
+{
+  # Basic iso stuff
+  imports = [
+    <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix>
+    <nixpkgs/nixos/modules/installer/cd-dvd/channel.nix>
+  ];
+}
